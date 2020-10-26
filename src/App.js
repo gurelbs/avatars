6d3c99fed13bf716c86b5c65e244b7b9753ff342
gurelbs <guri240@gmail.com>
2020-10-26 03:04:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Card from './Card'
+import contacts from './contects'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function createCard(contact){
+  return <Card 
+    key={contact.id}
+    imgSrc={contact.avatarImg}
+    name={contact.name}
+    address={contact.address}
+    phone={contact.phone}
+    birthday={contact.birthday}
+    email={contact.email}
+    lastDate={contact.lastDate}
+    quote={contact.quote}
+    quoteAuthor={contact.quoteAuthor}
+    score={contact.score}
+  />
 }
-
-export default App;
+export default function App() {
+  return ( 
+    <div>{contacts.map(createCard)}</div>
+  )
+}
---
+++ src/contects.js
@@ -0,0 +1,69 @@
+const contacts = [
+    {
+        id: 1,
+        name: 'Roberta M. Becerra',
+        avatarImg: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+        address: '1685 Eastland Avenue Jackson, MS 39201',
+        phone: '814-725-1627',
+        birthday: 'May 6, 1989',
+        email: 'LauraCToro@rhyta.com',
+        quote: '"You can see a lot by just looking."',
+        quoteAuthor: 'Yogi Berra',
+        lastDate: '',
+        score: 0
+    },
+    {
+        id: 2,
+        name: 'William D. Herzig',
+        avatarImg: "https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
+        address: '2901 Bolman Court Jacksonville, IL 62650',
+        phone: '217-370-4977',
+        birthday: 'February 20, 1978',
+        email: 'WilliamDHerzig@rhyta.com',
+        quote: '"The harder I work, the luckier I get."',
+        quoteAuthor: 'Gary Player',
+        lastDate: '',
+        score: 0
+    },
+    {
+        id: 3,
+        name: 'Betty T. Moss',
+        avatarImg: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1533&q=80",
+        address: '2871 Commerce Boulevard Mead, NE 68041',
+        phone: '402-624-5979',
+        birthday: 'August 10, 1998',
+        email: 'BettyTMoss@teleworm.us',
+        quote: '"Discovering the unexpected is more important than confirming the known."',
+        quoteAuthor: 'George Box',
+        lastDate: '',
+        score: 0
+    },
+    {
+        id: 4,
+        name: 'Darleen E. Mosqueda',
+        avatarImg: "https://images.unsplash.com/photo-1545912453-db258ca9b7b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+        address: '2272 Green Street Nashville, TN 37207',
+        phone: '615-513-1255',
+        birthday: 'October 18, 1985',
+        email: 'DarleenEMosqueda@dayrep.com',
+        quote: '"With the new day comes new strength and new thoughts."',
+        quoteAuthor: 'Eleanor Roosevelt',
+        lastDate: '',
+        score: 0
+    },
+    {
+        id: 5,
+        name: 'Leo S. Williams',
+        avatarImg: "https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1556&q=80",
+        address: '2714 Poe Lane Kansas City, KS 64108',
+        phone: '913-406-3698',
+        birthday: 'August 5, 1975',
+        email: 'LeoSWilliams@jourrapide.com',
+        quote: '"There is no elevator to success, you have to take the stairs."',
+        quoteAuthor: 'Zig Ziglars',
+        lastDate: '',
+        score: 0
+    }
+
+]
+export default contacts;
