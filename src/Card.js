0106a2b81ee8facd53f999af2958cfa22edaa4fc
gurelbs <guri240@gmail.com>
2020-10-26 04:33:14 +0200

    first commit

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,14 @@
+import React, {useState} from 'react'
 import './App.css';
-import React from 'react'
 
 export default function Card(props) {
+  let amountOfClick;
+  const [click, setClick] = useState(0);
+  function plusBtn(){
+    setClick(click+1);
+    amountOfClick = click+1;
+    console.log(amountOfClick);
+  }
   return (  
     <div className="card card-container row border-0">
       <div className="bg-dark text-light row px-0 mx-0">
@@ -9,32 +16,32 @@ export default function Card(props) {
           <img className="card-img-top px-0 mx-0" alt="avatar" src={props.imgSrc}/>
         </div>
         <div className="card-body p-0 col-8">
-          <h5 className="card-name col-12 pt-5 h1">{props.name}</h5>
+          <h5 className="card-name col-12 pt-5">{props.name}</h5>
         </div>
-        <div className="card-text col-5">
+        <div className="card-text col-8 col-xs-9">
           <blockquote className="blockquote">
             <p className="mb-0">{props.quote}</p>
             <footer className="blockquote-footer">{props.quoteAuthor}</footer>
           </blockquote>
         </div>
-        <div className="card-text  col-7">
-          <p className="p mb-0 mini-text address">
+        <div className="card-text col-4 col-xs-3">
+          <p className="p mb-0 mini-text address border-bottom py-1">
             <i className="fas fa-home"></i>&nbsp;
            {props.address}</p>
-          <p className="p mini-text card-text mb-0 ml-1">
+          <p className="p mini-text card-text mb-0 ml-1 border-bottom py-1">
             <i className="fas fa-phone-alt"></i>&nbsp;
           {props.phone}</p>
-          <p className="p mini-text card-text mb-0 ml-1">
+          <p className="p mini-text card-text mb-0 ml-1 border-bottom py-1">
             <i className="fas fa-birthday-cake"></i>&nbsp;
           {props.birthday}</p>
-          <p className="p mini-text card-text mb-0 ml-1">
+          <p className="p mini-text card-text mb-0 ml-1 border-bottom py-1">
             <i className="fas fa-mail-bulk"></i>&nbsp;
           {props.email}</p>
         </div>
-        <button type="button" className="btn btn-outline-light border-0  bg-light text-dark col-2 p-2 mb-0 like-btn">
+        <button onClick={plusBtn} type="button" className="btn btn-outline-light border-0  bg-light text-dark col-2 p-2 mb-1 ml-3 like-btn">
            <span><i className="far fa-thumbs-up"></i> </span>
-          <span className="badge col-2">
-          {props.score}
+          <span className="badge">
+          {click}
           </span>
         </button>
         <p className="badge my-auto text-muted col-10 pb-0 mb-0">Last Update: <span className="font-weight-light">{new Date().toLocaleString()}</span></p>     
