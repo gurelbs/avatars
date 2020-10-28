17eb8b849b356a30baa92cb8379bb03ab89d5283
gurelbs <guri240@gmail.com>
2020-10-28 10:51:14 +0200

    external map

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,29 @@ import contacts from './contects'
 
 export default function App() {
 
-let [contact,setContacts] = useState({contactsArr: [...contacts]});
-const score = x => {
-  let cardsArr = contact.contactsArr;
-  cardsArr[x].score++;
-  cardsArr[x].update = new Date().toLocaleString();
-  cardsArr.sort((a, b) => b.score - a.score);
-  setContacts(x => ({ ...x, contactsArr: cardsArr }));
-};
-  return (
-    <div className="m-1">
-    <Card 
-      key={contact.id}
-      cards={contact.contactsArr}
-      scoreUp={score}
-    />
-    </div>
+let [contact,setContacts] = useState([...contacts]
+    .sort((user1,user2) => user1.score - user2.score));
+const score = id => {
+  setContacts(
+    contact.map(user => {
+      if (user.id === id){
+        return {
+          ...user,
+          score: user.score + 1,
+          updateDate: new Date().toLocaleString()
+        }
+      }
+      return user;
+    }).sort((a, b) => b.score - a.score)
   )
+};
+  return <div className="m-1">
+    {contact.map(user => 
+      <Card 
+        key={user.id}
+        user={user}
+        scoreUp={score}
+      />
+      )}
+    </div>
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,54 +2,47 @@ import React from 'react'
 import './App.css';
 export default function Card(props) {
   return (
-
-    <div>
-{props.cards.map((card,key) => {
-        return ( 
-        <div key={key} className="card card-container row border-0">
+        <div key={props.key} className="card card-container row border-0">
           <div className="bg-dark text-light row px-0 mx-0">
           <div className="col-4">
-            <img className="card-img-top px-0 mx-0" alt="avatar" src={card.avatarImg}/>
+            <img className="card-img-top px-0 mx-0" alt="avatar" src={props.user.avatarImg}/>
           </div>
           <div className="card-body p-0 col-8">
-            <h5 className="card-name col-12">{card.name}</h5>
+            <h5 className="card-name col-12">{props.user.name}</h5>
           </div>
           <div className="text-quote  col-md-6 pt-3">
             <blockquote className="blockquote">
-              <p className="mb-0">{card.quote}</p>
-              <footer className="blockquote-footer">{card.quoteAuthor}</footer>
+              <p className="mb-0">{props.user.quote}</p>
+              <footer className="blockquote-footer">{props.user.quoteAuthor}</footer>
             </blockquote>
           </div>
           <div className="card-text col-md-6 pb-2">
             <p className="p mb-0 mini-text address border-bottom py-1">
               <i className="fas fa-home"></i>&nbsp;
-             {card.address}</p>
+             {props.user.address}</p>
             <p className="p mini-text card-text mb-0 ml-1 border-bottom py-1">
               <i className="fas fa-phone-alt"></i>&nbsp;
-            {card.phone}</p>
+            {props.user.phone}</p>
             <p className="p mini-text card-text mb-0 ml-1 border-bottom py-1">
               <i className="fas fa-birthday-cake"></i>&nbsp;
-            {card.birthday}</p>
+            {props.user.birthday}</p>
             <p className="p mini-text card-text mb-0 ml-1 border-bottom py-1">
               <i className="fas fa-mail-bulk"></i>&nbsp;
-            {card.email}</p>
+            {props.user.email}</p>
           </div>
           <button 
           onClick={() => {
-            props.scoreUp(key);
+            props.scoreUp(props.user.id);
           }} 
           type="button" 
           className=" badge btn btn-sm btn-outline-light border-0 mx-2 col-2 like-btn shadow-none">
             <span><i className="far fa-thumbs-up"></i>&nbsp;&nbsp;</span>
-          {card.score}</button>
+          {props.user.score}</button>
           <p className="badge my-auto text-muted col-9 pb-0 mb-0 ltr">Last Update:&nbsp;
-          <span className="font-weight-light">{!card.update ? 'Not updated yet' : card.update}</span>
+          <span className="font-weight-light">{!props.user.scoreUp ? 'Not updated yet' : props.user.scoreUp}</span>
           </p>     
           </div>
         </div> 
-        )
-      })}
-    </div>
   )
 }
 
